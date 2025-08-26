235f6d747dd5d8081080e3c2fc3daa8f579c78a2
Fett00 <musaew2014@gmail.com>
2025-08-26 15:50:17 +0300

    add prerelease and metadata indentifiers

--- Sources/swift-version/Version.swift
+++ Sources/swift-version/Version.swift
@@ -9,17 +9,51 @@ public struct Version: ExpressibleByStringLiteral {
     public let minor: UInt
     /// The patch version according to the semantic versioning standard.
     public let patch: UInt
+    /// The pre-release identifier according to the semantic versioning standard, starting with appending a hyphen and a series of dot separated identifiers
+    /// Examples: 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92, 1.0.0-x-y-z.--.
+    public let prereleaseIdentifiers: [String]
+    /// The build metadata of this version according to the semantic versioning standard, starting with appending a plus sign and a series of dot separated identifiers
+    /// Examples: 1.0.0-alpha+001, 1.0.0+20130313144700, 1.0.0-beta+exp.sha.5114f85, 1.0.0+21AF26D3----117B344092BD.
+    public let metadataIdentifiers: [String]
 
     public var stringRepresentation: String {
         "\(major).\(minor).\(patch)"
-    }
-
-    public var shortStringRepresentation: String {
-        "\(major)" + (minor > 0 ? ".\(minor)" : "") + (patch > 0 ? ".\(patch)" : "")
+        + (prereleaseIdentifiers.isEmpty ? "" : "\(prereleaseIdentifiers)")
+        + (metadataIdentifiers.isEmpty ? "" : "\(metadataIdentifiers)")
     }
 
     public init(stringLiteral value: StringLiteralType) {
-        let numbers = value.split(separator: ".").compactMap({ UInt($0) })
+
+        var editableValue = value
+
+        let beforePrereleaseIndex = value.firstIndex(of: "-")
+        let beforeMetadataIndex = value.firstIndex(of: "+")
+
+        if let beforeMetadataIndex {
+            let metadataIndex = editableValue.index(after: beforeMetadataIndex)
+            let metadataRange = metadataIndex ..< editableValue.endIndex
+            self.metadataIdentifiers = editableValue[metadataRange]
+                .split(separator: ".")
+                .compactMap({ String($0) })
+            editableValue
+                .removeSubrange(beforeMetadataIndex..<editableValue.endIndex) // replace. removing is too expansive
+        } else {
+            self.metadataIdentifiers = []
+        }
+
+        if let beforePrereleaseIndex {
+            let prereleaseIndex = editableValue.index(after: beforePrereleaseIndex)
+            let prereleaseRange = prereleaseIndex ..< editableValue.endIndex
+            self.prereleaseIdentifiers = editableValue[prereleaseRange]
+                .split(separator: ".")
+                .compactMap({ String($0) })
+            editableValue
+                .removeSubrange(beforePrereleaseIndex..<editableValue.endIndex) // replace. removing is too expansive
+        } else {
+            self.prereleaseIdentifiers = []
+        }
+
+        let numbers = editableValue.split(separator: ".").compactMap({ UInt($0) })
         switch numbers.count {
         case 1:
             (major, minor, patch) = (numbers[0], 0, 0)
@@ -32,10 +66,18 @@ public struct Version: ExpressibleByStringLiteral {
         }
     }
 
-    public init(major: UInt, minor: UInt, patch: UInt) {
+    public init(
+        major: UInt,
+        minor: UInt,
+        patch: UInt,
+        prereleaseIdentifiers: [String] = [],
+        metadataIdentifiers: [String] = []
+    ) {
         self.major = major
         self.minor = minor
         self.patch = patch
+        self.prereleaseIdentifiers = prereleaseIdentifiers
+        self.metadataIdentifiers = metadataIdentifiers
     }
 }
 
@@ -43,10 +85,12 @@ extension Version: Comparable {
 
     public static func < (lhs: Version, rhs: Version) -> Bool {
         lhs.major < rhs.major && lhs.minor < rhs.minor && lhs.patch < rhs.patch
+        // TODO: add metadata and prerelease to comparsion
     }
 
     public static func ==(lhs: Version, rhs: Version) -> Bool {
         lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch == rhs.patch
+        // TODO: add metadata and prerelease to equation
     }
 }
 
@@ -57,6 +101,8 @@ extension Version: CustomStringConvertible, CustomDebugStringConvertible {
 
     public var debugDescription: String {
         "Version: major \(major), minor \(minor), patch \(patch)"
+        + (prereleaseIdentifiers.isEmpty ? "" : ", preReleaseIdentifiers: \(prereleaseIdentifiers)")
+        + (metadataIdentifiers.isEmpty ? "" : ", metadataIdentifiers: \(metadataIdentifiers)")
     }
 }
 
